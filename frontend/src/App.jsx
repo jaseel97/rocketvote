7ffb74de7e259361c5a3719a4f95f51b341c97db
AbhishekGoddale <abhigoddale@gmail.com>
2024-09-25 12:49:32 -0400

    Frontend Boilerplate

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  return (
+    <h1 className="text-3xl font-bold underline">
+      OLDMAN = JASEEL
+    </h1>
+  )
+
+}
+
+export default App
