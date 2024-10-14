2f699cf560f28cf13c8fc6dc8d91a32b130292c3
jaseel <harisj@uwindsor.ca>
2024-10-14 17:58:33 -0400

    changes to parameterize docker stuff

---
+++ backend/start.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+python manage.py migrate
+python manage.py createsuperuser --noinput || echo "Superuser already exists, skipping creation."
+
+# CMD ["gunicorn", "--workers", "3", "--bind", "0.0.0.0:8080", "rocketVoteAPI.wsgi:application"]
+exec gunicorn --workers ${GUNICORN_WORKERS} --bind "0.0.0.0:${API_PORT}" rocketVoteAPI.wsgi:application
