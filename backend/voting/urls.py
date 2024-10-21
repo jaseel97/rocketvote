cc77f0932baaddb1ad1a3562937f9559e1a9c0f0
jaseel <harisj@uwindsor.ca>
2024-10-20 20:53:28 -0400

    added poll admin API

--- backend/voting/urls.py
+++ backend/voting/urls.py
@@ -1,9 +1,11 @@
-from django.urls import path
+from django.urls import include, path
+import django_eventstream
 
 from . import views
 
 urlpatterns = [
     path("templates", views.templates, name="index"),
     path("create", views.create, name="create_poll"),
-    path("<str:poll_id>", views.cast_vote, name="cast_vote")
+    path("create/<str:creation_id>", views.poll_admin, name="poll_admin"),
+    path("<str:poll_id>", views.cast_vote, name="cast_vote"),
 ]
--- backend/voting/views.py
+++ backend/voting/views.py
@@ -4,7 +4,7 @@ from django.views.decorators.csrf import csrf_exempt
 import json
 from nanoid import generate
 
-from .utils import get_poll
+from .utils import get_poll, get_poll_from_creation_id, get_poll_results, make_poll_metadata_string
 from .models import PollTemplate
 from .redis_pool import get_redis_connection
 
@@ -71,71 +71,71 @@ def templates(request):
 # takes the poll meta data from the UI and creates a new poll
 @csrf_exempt
 def create(request):
+    if request.method != 'POST':
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
+
     try:
-        if request.method != 'POST':
-            return HttpResponseBadRequest('Invalid request method')
+        poll_body = json.loads(request.body)
+    except json.JSONDecodeError:
+        return JsonResponse({'error': 'Invalid JSON payload'}, status=400)
 
-        try:
-            poll_body = json.loads(request.body)
-        except json.JSONDecodeError:
-            return HttpResponseBadRequest('Invalid JSON payload')
+    required_fields = ['type', 'revealed', 'multi_selection', 'options']
+    if not all(field in poll_body for field in required_fields):
+        return JsonResponse({'error': 'Missing required fields'}, status=400)
 
-        if not all(field in poll_body for field in required_fields):
-            return HttpResponseBadRequest('Missing required fields')
+    if not isinstance(poll_body['options'], list) or len(poll_body['options']) == 0:
+        return JsonResponse({'error': 'Options must be a non-empty list'}, status=400)
 
-        if not isinstance(poll_body['options'], list) or len(poll_body['options']) == 0:
-            return HttpResponseBadRequest('Options must be a non-empty list')
+    creation_id = generate()
+    new_poll_id = generate(size=7)  # for shareable URL
 
-        creation_id = generate()
-        new_poll_id = generate(size=7) # for shareable url
+    poll_metadata_key = f'{new_poll_id}:metadata'
+    options = "-:-".join(poll_body['options'])
+    poll_metadata = f"{poll_body['type']}-;-{poll_body['revealed']}-;-{poll_body['multi_selection']}-;-{options}"
 
-        poll_metadata_key = f'{new_poll_id}:metadata'
-        options = "-:-".join(poll_body['options'])
-        poll_metadata = f"{poll_body['type']}-;-{poll_body['revealed']}-;-{poll_body['multi_selection']}-;-{options}"
-
-        try:
-            redis_conn = get_redis_connection()
-            redis_conn.set(poll_metadata_key, poll_metadata)
-            creation_to_poll_key = f'{creation_id}:poll_id'
-            redis_conn.set(creation_to_poll_key, new_poll_id)
-        except Exception as e:
-            return HttpResponseServerError(f"Failed to save poll data: {str(e)}")
-
-        return JsonResponse(
-            {
-                'poll_id' : new_poll_id,
-                'redirect_url': f'/create/{creation_id}'
-            }, 
-            status=302
-        )
-
+    try:
+        redis_conn = get_redis_connection()
+        redis_conn.set(poll_metadata_key, poll_metadata)
+        creation_to_poll_key = f'{creation_id}:poll_id'
+        redis_conn.set(creation_to_poll_key, new_poll_id)
     except Exception as e:
-        return HttpResponseServerError(f"An unexpected error occurred: {str(e)}")
+        return JsonResponse({'error': f'Failed to save poll data: {str(e)}'}, status=500)
+
+    return JsonResponse(
+        {
+            'poll_id': new_poll_id,
+            'redirect_url': f'/create/{creation_id}'
+        },
+        status=201
+    )
 
 @csrf_exempt
 def cast_vote(request, poll_id):
     if request.method != 'PATCH':
-        return HttpResponseBadRequest('Invalid request method')
-    
-    poll = get_poll(poll_id)
-    if poll is None or poll['revealed'] == '1':
-        return HttpResponseBadRequest('Poll Expired/Ended')
+        return JsonResponse({'error': 'Invalid request method'}, status=400)
+
+    redis_conn = get_redis_connection()
+
+    try:
+        poll = get_poll(redis_conn, poll_id)
+        if poll is None or poll['revealed'] == '1':
+            return JsonResponse({'error': 'Poll Expired/Ended'}, status=400)
+    except:
+        return JsonResponse({'error':'An unexpected error has occured'}, status=500)
     
     try:
         ballot = json.loads(request.body)
     except json.JSONDecodeError:
-        return HttpResponseBadRequest('Invalid JSON payload')
+        return JsonResponse({'error': 'Invalid JSON payload'}, status=400)
     
     if not all(field in poll['options'] for field in ballot['votes']):
-        return HttpResponseBadRequest('Invalid option')
+        return JsonResponse({'error': 'Invalid option'}, status=400)
 
     if len(ballot['votes']) != len(set(ballot['votes'])):
-        return HttpResponseBadRequest('Duplicate votes are not allowed')
+        return JsonResponse({'error': 'Duplicate votes are not allowed'}, status=400)
 
-    try:
-        ballot['voter']
-    except:
-        return HttpResponseBadRequest('Missing username')
+    if 'voter' not in ballot:
+        return JsonResponse({'error': 'Missing username'}, status=400)
     
     poll_votes_key = f'{poll_id}:votes'
     poll_count_key = f'{poll_id}:count'
@@ -144,8 +144,6 @@ def cast_vote(request, poll_id):
     new_votes = '-:-'.join(ballot['votes'])
 
     try:
-        redis_conn = get_redis_connection()
-
         prev_votes = redis_conn.hget(poll_votes_key, voter_name)
         if prev_votes:
             prev_votes = prev_votes.decode('utf-8').split('-:-')
@@ -157,7 +155,42 @@ def cast_vote(request, poll_id):
         for option in ballot['votes']:
             redis_conn.zincrby(poll_count_key, 1, option)
     except Exception as e:
-        return HttpResponseServerError(f"Failed to save poll data: {str(e)}")
+        return JsonResponse({'error': f'Failed to save poll data: {str(e)}'}, status=500)
     
-    return HttpResponse('Vote/s cast successfully', 200)
+    return JsonResponse({'message': 'Vote/s cast successfully'}, status=200)
 
+@csrf_exempt
+def poll_admin(request, creation_id):
+    redis_conn = get_redis_connection()
+    if request.method == "GET":
+        poll_id, poll = get_poll_from_creation_id(redis_conn, creation_id)
+        if poll is None:
+            return JsonResponse({'error': 'Invalid creation ID'}, status=400)
+        
+        poll_results = get_poll_results(redis_conn, poll_id)
+        
+        response = {
+            'metadata': poll,
+            'votes' : poll_results[0],
+            'counts' : poll_results[1]
+        }
+        print("Response : ", response)
+        return JsonResponse(response, status=200)
+    elif request.method == "PATCH":
+        poll_id, poll = get_poll_from_creation_id(redis_conn, creation_id)
+        if poll is None:
+            return JsonResponse({'error': 'Invalid creation ID'}, status=400)
+        
+        poll['revealed'] = 1
+        poll_metadata = make_poll_metadata_string(poll)
+        poll_metadata_key = f'{poll_id}:metadata'
+        
+        try:
+            redis_conn.set(poll_metadata_key, poll_metadata)
+            #TODO : send event through WS
+        except Exception as e:
+            return JsonResponse({'error': f'Failed to update poll data: {str(e)}'}, status=500)
+
+        return JsonResponse({'message':'Poll results revealed'}, status=200)
+
+    return JsonResponse({'error': 'Invalid request method'}, status=400)
