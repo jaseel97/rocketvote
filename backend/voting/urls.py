8bd33aa12c32537c537b65fee8fd3c6237275e21
jaseel <harisj@uwindsor.ca>
2024-10-21 23:42:10 -0400

    added poll get api for participants

--- backend/voting/urls.py
+++ backend/voting/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path("templates", views.templates, name="index"),
     path("create", views.create, name="create_poll"),
     path("create/<str:creation_id>", views.poll_admin, name="poll_admin"),
-    path("<str:poll_id>", views.cast_vote, name="cast_vote"),
+    path("<str:poll_id>", views.cast_vote, name="participant_functions"),
 ]
