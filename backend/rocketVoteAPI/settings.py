16bb3d6564edfb4ce598586d74c343b955823d96
jaseel <harisj@uwindsor.ca>
2024-10-24 12:51:24 -0400

    nginx setup

--- backend/rocketVoteAPI/settings.py
+++ backend/rocketVoteAPI/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-705i4w^m5kfy7l-!qrci&clg(@jq2(^&5qpgl&0v0!s$8hi#l2
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv('DJANGO_ALLOWED_HOSTS', '').split(',')
 
 
 # Application definition
