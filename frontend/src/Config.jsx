2951f6722ee6b5c230c96850808beeeb317e9453
jaseel <harisj@uwindsor.ca>
2024-12-04 17:01:09 -0500

    domain changes

--- frontend/src/Config.jsx
+++ frontend/src/Config.jsx
@@ -1,8 +1,8 @@
 
-export const appDomain = "https://rocketvote.com"     //http://localhost
-export const apiDomain = "https://rocketvote.com/api" //http://localhost:8080
-export const wsDomain  = "wss://rocketvote.com/ws"    //ws://http://localhost:8080/ws
+export const appDomain = "https://rocketvote.xyz"     //http://localhost
+export const apiDomain = "https://rocketvote.xyz/api" //http://localhost:8080
+export const wsDomain  = "wss://rocketvote.xyz/ws"    //ws://http://localhost:8080/ws
 
 export const ENTRA_TENANT_ID = 'adf55c08-b67b-4701-ad38-5cca21d455b0' //import.meta.env.VITE_ENTRA_TENANT_ID
 export const ENTRA_CLIENT_ID = 'f647dc60-9ca8-4a99-ae7a-7f232d23cd79'  //import.meta.env.VITE_ENTRA_CLIENT_ID
-export const ENTRA_REDIRECT_URI = 'https://rocketvote.com/api/oauth2/callback'
+export const ENTRA_REDIRECT_URI = 'https://rocketvote.xyz/api/oauth2/callback'
